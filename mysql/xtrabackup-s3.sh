c19e4f3b9b59874a42d97f34a6e727c88747181b
Ivo Marino <ivo@floads.io>
2024-10-24 17:58:02 +0200

    Initial restore logic

--- mysql/xtrabackup-s3.sh
+++ mysql/xtrabackup-s3.sh
@@ -7,7 +7,7 @@
 # % xtrabackup-s3.sh inc        Make an incremental backup to S3
 #
 # Restore a full + incrementals:
-# % xtrabackup-s3.sh restore <path-to-full-backup> <path-to-inc-backup-1> <path-to-inc-backup-2>
+# % xtrabackup-s3.sh restore <full-backup> <inc-backup-1> <inc-backup-2>
 #
 
 #
@@ -44,42 +44,52 @@ CFG_INCREMENTAL=""
 OPT_BACKUP_TYPE="${1:-}"
 OPT_DRY_RUN="${2:-}"
 
-# See https://docs.percona.com/percona-xtrabackup/2.4/xbcloud/xbcloud.html#preparing-an-incremental-backup
-# restore requires:
-#   - a path to a full backup
-#   - 
-
-if [ "${OPT_BACKUP_TYPE}" != "full" ] && [ "${OPT_BACKUP_TYPE}" != "inc" ]; then
+if [ "${OPT_BACKUP_TYPE}" != "full" ] && [ "${OPT_BACKUP_TYPE}" != "inc" ] && [ "${OPT_BACKUP_TYPE}" != "restore" ]; then
     echo "Usage: $0 {full|inc} [--dry-run]"
+    echo "       $0 restore <full-backup> <inc-backup-1> <inc-backup-2> [--dry-run]"
     exit 1
 fi
 
-if [ ! -d "${CFG_EXTRA_LSN_DIR}" ]; then
-    echo "Creating local LSN directory: ${CFG_EXTRA_LSN_DIR}"
-    mkdir -p "${CFG_EXTRA_LSN_DIR}"
-fi
+# we backup, full or inc
+if [ "${OPT_BACKUP_TYPE}" = "full" ] || [ "${OPT_BACKUP_TYPE}" = "inc" ]; then
 
-if [ "${OPT_BACKUP_TYPE}" = "inc" ]; then
-    if [ ! -f "${CFG_EXTRA_LSN_DIR}/xtrabackup_checkpoints" ]; then
-        echo "No previous full backup found. Please run a full backup first."
-        exit 1
-    fi
-    CFG_INCREMENTAL="--incremental-basedir=${CFG_EXTRA_LSN_DIR}"
-fi
-
-if [ -n "${OPT_DRY_RUN}" ]; then
-    echo "Dry run: xtrabackup --backup ${CFG_INCREMENTAL} --extra-lsndir=${CFG_EXTRA_LSN_DIR} --stream=xbstream --target-dir=${CFG_EXTRA_LSN_DIR} | \
-xbcloud put ${CFG_HOSTNAME}/${CFG_DATE}-${OPT_BACKUP_TYPE}_${CFG_TIMESTAMP}"
-else
-    xtrabackup --backup ${CFG_INCREMENTAL} --extra-lsndir=${CFG_EXTRA_LSN_DIR} --stream=xbstream --target-dir=${CFG_EXTRA_LSN_DIR} | \
-xbcloud put ${CFG_HOSTNAME}/${CFG_DATE}-${OPT_BACKUP_TYPE}_${CFG_TIMESTAMP}
-
-    if [ $? -ne 0 ]; then
-        echo "Backup failed!"
-        exit 1
+    if [ ! -d "${CFG_EXTRA_LSN_DIR}" ]; then
+        echo "Creating local LSN directory: ${CFG_EXTRA_LSN_DIR}"
+        mkdir -p "${CFG_EXTRA_LSN_DIR}"
     fi
 
-    echo "Backup completed successfully."
+    if [ "${OPT_BACKUP_TYPE}" = "inc" ]; then
+        if [ ! -f "${CFG_EXTRA_LSN_DIR}/xtrabackup_checkpoints" ]; then
+            echo "No previous full backup found. Please run a full backup first."
+            exit 1
+        fi
+        CFG_INCREMENTAL="--incremental-basedir=${CFG_EXTRA_LSN_DIR}"
+    fi
+
+    if [ -n "${OPT_DRY_RUN}" ]; then
+        echo "Dry run: xtrabackup --backup ${CFG_INCREMENTAL} --extra-lsndir=${CFG_EXTRA_LSN_DIR} --stream=xbstream --target-dir=${CFG_EXTRA_LSN_DIR} | \
+    xbcloud put ${CFG_HOSTNAME}/${CFG_DATE}-${OPT_BACKUP_TYPE}_${CFG_TIMESTAMP}"
+    else
+        xtrabackup --backup ${CFG_INCREMENTAL} --extra-lsndir=${CFG_EXTRA_LSN_DIR} --stream=xbstream --target-dir=${CFG_EXTRA_LSN_DIR} | \
+    xbcloud put ${CFG_HOSTNAME}/${CFG_DATE}-${OPT_BACKUP_TYPE}_${CFG_TIMESTAMP}
+
+        if [ $? -ne 0 ]; then
+            echo "Backup failed!"
+            exit 1
+        fi
+
+        echo "Backup completed successfully."
+    fi
+
+# we restore
+elif [ "${OPT_BACKUP_TYPE}" = "restore" ]; then
+
+    # See https://docs.percona.com/percona-xtrabackup/2.4/xbcloud/xbcloud.html#preparing-an-incremental-backup
+    # restore requires:
+    #   - a path to a full backup
+    #   - 
+
+    echo "Do a restore..."
 fi
 
 exit 0
