b4499508261167a915c52f2b07cd9101ea70e0ed
Ivo Marino <ivo@floads.io>
2024-11-05 14:21:37 +0100

    Updated

--- linux/debian/update.sh
+++ linux/debian/update.sh
@@ -2,12 +2,12 @@
 
 # Update packages without restarting services
 
-DEBIAN_FRONTEND=noninteractive
-
-apt-get -o Dpkg::Options::="--force-confold" \
+DEBIAN_FRONTEND=noninteractive \
+    apt-get -o Dpkg::Options::="--force-confold" \
     -o Dpkg::Options::="--force-confdef" \
     -o DPkg::options::="--force-confnew" dist-upgrade -y $1
 
-apt-get -o Dpkg::Options::="--force-confold" \
+DEBIAN_FRONTEND=noninteractive \
+    apt-get -o Dpkg::Options::="--force-confold" \
     -o Dpkg::Options::="--force-confdef" \
     -o DPkg::options::="--force-confnew" auto-remove -y $1
