32e7ce7a6d007d453f729c1efdbc361c5c442661
Ivo Marino <ivo@floads.io>
2024-11-05 13:46:33 +0100

    Initial import

---
+++ linux/debian/update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Update packages without restarting services
+
+DEBIAN_FRONTEND=noninteractive
+
+apt-get -o Dpkg::Options::="--force-confold" \
+    -o Dpkg::Options::="--force-confdef" \
+    -o DPkg::options::="--force-confnew" dist-upgrade -y $1
+
+apt-get -o Dpkg::Options::="--force-confold" \
+    -o Dpkg::Options::="--force-confdef" \
+    -o DPkg::options::="--force-confnew" auto-remove -y $1
