1ecae7501a4fbdceac14db28955eb46461ad2cd4
Ivo Marino <ivo@floads.io>
2025-01-24 13:46:36 +0100

    Support for release and all jails

--- bsd/freebsd/upgrade-jails.sh
+++ bsd/freebsd/upgrade-jails.sh
@@ -2,7 +2,11 @@
 
 # https://alfaexploit.com/en/posts/managing_jails_in_freebsd_with_bastille/#update
 
-for JAIL_PATH in /usr/local/bastille/jails/*; do JAIL=$(basename $JAIL_PATH) && echo -n "-- $JAIL: " && grep osrelease $JAIL_PATH/jail.conf >/dev/null 2>/dev/null ; if [ $? -eq 0 ]; then echo "Thin jail" ; else echo "Thick jail"; fi ; done
+for JAIL_PATH in /usr/local/bastille/jails/*; do
+    JAIL=$(basename $JAIL_PATH) && echo -n "-- $JAIL: " \
+        && grep osrelease $JAIL_PATH/jail.conf >/dev/null 2>/dev/null ; \
+        if [ $? -eq 0 ]; then echo "Thin jail" ; else echo "Thick jail"; fi ;
+done
 
 yes | sudo bastille pkg $1 update -f
 sudo bastille pkg $1 upgrade -y
