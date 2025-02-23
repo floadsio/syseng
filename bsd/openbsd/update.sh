c7fb079d50b7dacbae3236540186f5a20703d849
Ivo Marino <ivo@floads.io>
2025-02-23 13:14:16 +0100

    Reverted to pkg_add

--- bsd/openbsd/update.sh
+++ bsd/openbsd/update.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 /usr/sbin/syspatch
-# /usr/sbin/pkg_add -Uu
-/usr/local/bin/pkgupdate
+
+/usr/sbin/pkg_add -Uu
+# /usr/local/bin/pkgupdate
 
 # do this when 7.5 is released
 # sysupgrade -r -n
