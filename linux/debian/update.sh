70bb50684ea01820e3b33c99be92dec2a6f97af8
Ivo Marino <ivo@floads.io>
2025-04-02 19:26:08 +0200

    More steps

--- linux/debian/update.sh
+++ linux/debian/update.sh
@@ -2,6 +2,13 @@
 
 # Update packages without restarting services
 
+apt-get update
+
+DEBIAN_FRONTEND=noninteractive \
+    apt-get -o Dpkg::Options::="--force-confold" \
+    -o Dpkg::Options::="--force-confdef" \
+    -o DPkg::options::="--force-confnew" upgrade -y $1
+
 DEBIAN_FRONTEND=noninteractive \
     apt-get -o Dpkg::Options::="--force-confold" \
     -o Dpkg::Options::="--force-confdef" \
